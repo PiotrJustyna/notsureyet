26be9f0ac64434d1cd80c036dabd94ea1d61f5fe
Piotr Justyna <piotr.justyna@gmail.com>
2016-08-20 23:55:06 +0100

    Communication between threads.

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,57 @@
 module Main where
 
 import Control.Concurrent
+import Control.Concurrent.Chan
+import Control.Exception
+import Control.Monad (liftM, when)
+import Control.Monad.Fix (fix)
 import Network.Socket
 import System.IO
 
+type Message = (Int, String)
+
 main :: IO ()
-main = withSocketsDo $ do
+main = do
   socket <- socket AF_INET Stream 0
   setSocketOption socket ReuseAddr 1
   bind socket $ SockAddrInet 4242 iNADDR_ANY
   listen socket 2
-  mainLoop socket
+  channel <- newChan
+  forkIO $ fix $ \loop -> do
+    (_, msg) <- readChan channel
+    loop
+  mainLoop socket channel 0
 
-mainLoop :: Socket -> IO ()
-mainLoop socket = do
-    connection <- accept socket
-    forkIO $ connectionHandler connection
-    mainLoop socket
+mainLoop :: Socket -> Chan Message -> Int -> IO ()
+mainLoop socket channel messageNumber = do
+  connection <- accept socket
+  forkIO $ connectionHandler connection channel messageNumber
+  mainLoop socket channel $! messageNumber + 1
 
-connectionHandler :: (Socket, SockAddr) -> IO ()
-connectionHandler (socket, _) = do
-    ioHandle <- socketToHandle socket ReadWriteMode
-    hSetBuffering ioHandle NoBuffering
-    hPutStrLn ioHandle "Hello!"
-    hClose ioHandle
+connectionHandler :: (Socket, SockAddr) -> Chan Message -> Int -> IO ()
+connectionHandler (socket, _) channel messageNumber = do
+  let broadcast message = writeChan channel (messageNumber, message)
+  ioHandle <- socketToHandle socket ReadWriteMode
+  hSetBuffering ioHandle NoBuffering
+
+  hPutStrLn ioHandle "Hi, what's your name?"
+  name <- liftM init (hGetLine ioHandle)
+  broadcast ("--> " ++ name ++ " entered chat.")
+  hPutStrLn ioHandle ("Welcome, " ++ name ++ "!")
+
+  commLine <- dupChan channel
+
+  reader <- forkIO $ fix $ \loop -> do
+    (nextNumber, line) <- readChan commLine
+    when (messageNumber /= nextNumber) $ hPutStrLn ioHandle line
+    loop
+
+  handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
+    line <- liftM init (hGetLine ioHandle)
+    case line of
+      "quit" -> hPutStrLn ioHandle "Bye!"
+      _ -> broadcast (name ++ ": " ++ line) >> loop
+  killThread reader
+  broadcast ("<-- " ++ name ++ " left.")
+
+  hClose ioHandle
