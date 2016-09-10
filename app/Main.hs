56741264dc22f3c700e36ade2477308d9858831d
Piotr Justyna <piotr.justyna@gmail.com>
2016-09-10 11:37:41 +0100

    Minor experiments.

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,7 @@
 module Main where
 
-import Control.Concurrent
-import Network.Socket
-import System.IO
+import Network.Simple.TCP
 
 main :: IO ()
-main = do
-  socket <- socket AF_INET Stream 0
-  setSocketOption socket ReuseAddr 1
-  bind socket $ SockAddrInet 4242 iNADDR_ANY
-  listen socket 2
-  mainLoop socket
-
-mainLoop :: Socket -> IO ()
-mainLoop socket = do
-    connection <- accept socket
-    forkIO $ connectionHandler connection
-    mainLoop socket
-
-connectionHandler :: (Socket, SockAddr) -> IO ()
-connectionHandler (socket, _) = do
-    ioHandle <- socketToHandle socket ReadWriteMode
-    hSetBuffering ioHandle NoBuffering
-    hPutStrLn ioHandle "Hello!"
-    hClose ioHandle
+main = serve (Host "127.0.0.1") "4242" $ \(connectionSocket, remoteAddr) -> do
+  putStrLn $ "TCP connection established from " ++ show remoteAddr
