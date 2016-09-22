327a05c85953e66eaa01dce20ea546e4b92f43c8
Piotr Justyna <piotr.justyna@gmail.com>
2016-09-22 22:07:36 +0100

    Added message loop for every connection.

--- app/Main.hs
+++ app/Main.hs
@@ -15,8 +15,13 @@ handleOpenedConnection :: (Socket, SockAddr) -> IO ()
 handleOpenedConnection (socket, remoteAddress) = do
   message <- receiveMessage socket
   case message of
-    Just x -> putStrLn $ (show remoteAddress) ++ ": " ++ (show x)
-    Nothing -> putStrLn $ (show remoteAddress) ++ ": -"
+    Just x ->
+      if (show x) == "\"\\ESC\""
+        then putStrLn $ (show remoteAddress) ++ ": client wants to close the connection"
+        else do
+          putStrLn $ (show remoteAddress) ++ ": " ++ (show x)
+          handleOpenedConnection (socket, remoteAddress)
+    Nothing -> putStrLn $ (show remoteAddress) ++ ": connection lost"
 
 receiveMessage :: Socket -> IO (Maybe BS.ByteString)
 receiveMessage socket = recv socket 1
